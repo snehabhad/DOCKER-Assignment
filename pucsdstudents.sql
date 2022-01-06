93c12260f9930418918a51065dddea5097aaa31d
snehabhad <86283920+snehabhad@users.noreply.github.com>
2022-01-06 20:45:17 +0530

    Update pucsdstudents.sql

--- pucsdstudents.sql
+++ pucsdstudents.sql
@@ -8,18 +8,13 @@ CREATE TABLE StudentsData (RollNo int,Name varchar(20),Address varchar(20),Mobil
 
 INSERT INTO StudentsData(RollNo, Name, Address , Mobile_Number,PAN_Number) VALUES (101, 'Mitali', 'Pune', '9917512531', 'AC2341WTS');
 
-
 INSERT INTO StudentsData(RollNo, Name, Address , Mobile_Number,PAN_Number) VALUES (102, 'Shivam', 'Nagar', '9517512531', 'CS2341WTS');
-
 
 INSERT INTO StudentsData(RollNo, Name, Address , Mobile_Number,PAN_Number) VALUES (103, 'Priya', 'Nashik', '8817512531', 'TU2341WTS');
 
-
 INSERT INTO StudentsData(RollNo, Name, Address , Mobile_Number,PAN_Number) VALUES (104, 'Pratik', 'Nashik', '9817512531', 'TC2341WTS');
 
-
 INSERT INTO StudentsData(RollNo, Name, Address , Mobile_Number,PAN_Number) VALUES (105, 'Mayur', 'Nashik', '9417512531', 'AC2341WTS');
-
 
 INSERT INTO StudentsData(RollNo, Name, Address , Mobile_Number,PAN_Number) VALUES (106, 'Prajakta', 'Nagar', '8165315122', 'ABCD123411');
 
